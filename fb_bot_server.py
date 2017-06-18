d1614b515da6533429e41f1572972c0710963f90
Daxeel Soni <daxeel_soni4444@yahoo.com>
2017-06-18 12:40:07 +0530

    Final code for fb bot

--- fb_bot_server.py
+++ fb_bot_server.py
@@ -9,8 +9,8 @@ from flask import Flask, request
 app = Flask(__name__)
 
 # Facebook credentials
-PAGE_ACCESS_TOKEN = "EAAVNSagZBUMIBAMb89moYytPBZBBU8ZAZBHT34wJY9CyMUB9XedF5RihfeXmSmgZB5PDEKmWpErUTikCHKfaJcbAZBwa8ALKgPVG2tuiDvp4x0X4MkOgO4gnZC4TmfKPmAGBnKMgcPuMeXs1vgsq9yhzZAqpDcs1THZB8yHMZACUc82AZDZD"
-VERIFY_TOKEN = "gdg_bot_token"
+PAGE_ACCESS_TOKEN = "PAGE_TOKEN"
+VERIFY_TOKEN = "VERIFY_TOKEN"
 
 # Chatbot verification from facebook app
 @app.route('/', methods=['GET'])
@@ -20,7 +20,7 @@ def verify():
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
 
-    return "Hello world", 200
+    return "Working!", 200
 
 # Process incoming messages
 @app.route('/', methods=['POST'])
@@ -35,19 +35,16 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-                  
-                    link = requests.get("http://memes-kashyapb.rhcloud.com/"+str(message_text)).text
-                    image_link = json.loads(link)
-                    image_link = image_link['data'][0]['link']
-
-                    if message_text in ["hi", "hello", "hola"]:
-                        send_message(sender_id, "Hello form GDG Meetup!")
-                    else:
-                        if len(image_link) > 0:
-                            send_image(sender_id, image_link)
-                        else:
-                            send_message(sender_id, "Still Learning.")
                     
+                    # Calling dictionary API
+                    res = json.loads(requests.get("https://api.import.io/store/connector/debc3d00-bab7-408a-9c3f-339beff62703/_query?input=webpage%2Furl%3Ahttps%3A%2F%2Fglosbe.com%2Fen%2Fen%2Fcomputer&&_apikey=5b5fd39b293d467a96a3ae991b8af88bf05390ea32dede72c808e95892bd1eff9549aed258aa36b1c0beca77e7d58f9efd66f8680ba152e7f7599f1e7056cd8f211ae67c876c4107da3a9e19a7b396ff").text)
+                    definition = res['results'][0]['description']
+
+                    if len(definition) > 0:
+                        send_message(sender_id, "Searching...")
+                        send_message(sender_id, definition)
+                    else:
+                        send_message(sender_id, "Not found.")
 
     return "ok", 200
 
@@ -68,30 +65,6 @@ def send_message(recipient_id, message_text):
         }
     })
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
-
-# Method to send images
-def send_image(recipient_id, img_url):
-    params = {
-        "access_token": PAGE_ACCESS_TOKEN
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
-    data = json.dumps({
-        "recipient": {
-            "id": recipient_id
-        },
-        "message": {
-            "attachment": {
-                "type":"image",
-                "payload": {
-                    "url": img_url
-                }
-            }
-        }
-    })
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
-
 
 # Run flask server
 if __name__ == '__main__':
